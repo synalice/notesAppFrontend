e797ad26ded78aebc84f18e0e572b92a7217e6b0
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2024-02-09 17:23:25 +0300

    feat: add alert components

---
+++ components/Alert/Danger.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="alert alert-danger d-flex align-items-center gap-2" role="alert">
+    <IconExclamationTriangle />
+    <div>
+      <slot name="text">Это уведемление с сообщением об ошибке</slot>
+    </div>
+    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+  </div>
+</template>
---
+++ components/Alert/Warning.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="alert alert-warning d-flex align-items-center gap-2" role="alert">
+    <IconExclamationTriangle />
+    <div>
+      <slot name="text">Это уведемление с предупреждением</slot>
+    </div>
+    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+  </div>
+</template>
